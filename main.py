6f4efde440b0e6791ee9609dd9105cbcf0517b15
saf <safwaanshaikh21@gmail.com>
2024-10-13 04:30:54 -0700

    mirror properly (i hope)

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from typing import Dict, Any
-
+import json
 app = FastAPI()
 
 @app.post("/sendData/")
-async def send_data(data: Dict[str, Any]):
+async def send_data(data: str):
     """
     Accepts JSON input and returns the same JSON as a response.
     """
+    body = json.loads(data)
     return {"received_data": data}
