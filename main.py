bdf9500e2c4d7fe02ed46deb04408d15fd5b2f1e
saf <safwaanshaikh21@gmail.com>
2024-10-13 05:25:59 -0700

    remove parse

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ from typing import Dict, Any
 import json
 app = FastAPI()
 
+# class Item(BaseModel):
+#     name: str
+#     price: float
+
+# @app.post("/items/")
+# def create_item(item: Item):
+#     return item  # FastAPI automatically converts this to JSON
+
 @app.post("/sendData/")
 async def send_data(data: str):
+    print(f"[safwaans-test]: {data}")
     """
     Accepts JSON input and returns the same JSON as a response.
     """
-    body = json.loads(data)
-    return {"received_data": data}
+    return {"received_data": data["head"]}
