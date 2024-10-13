96a9509d80799af0da25cfafec62db49b8ae8898
saf <safwaanshaikh21@gmail.com>
2024-10-13 11:43:41 -0700

    think

--- main.py
+++ main.py
@@ -14,26 +14,25 @@ async def send_data(request: Request):
     """
     Accepts raw JSON input and returns the 'head' part of it.
     """
-    try:
-        data = await request.json()  # Parse the incoming JSON request body
-        logger.info(f"Received data: {data}")
-        # Print the type and check if it's a string or a dictionary
+    logger.info(f"JSONas")
+    data = await request.json()  # Parse the incoming JSON request body
+    logger.info(f"Received data: {data}")
+    # Print the type and check if it's a string or a dictionary
 
-        if type(data) == str:
-            return {"head": "String"}
-        else:
-            return {"head": data["head"].keys()}
+    if type(data) == str:
+        return {"head": "String"}
+    else:
+        return {"head": data["head"].keys()}
 
-        return {"head": data["head"]}
-        input_data = np.array(data["data"]).reshape(1, -1)
+    return {"head": data["head"]}
+    input_data = np.array(data["data"]).reshape(1, -1)
 
-        with open("posture_model.pkl", "rb") as f:
-            loaded_model = load(f)
-            res = loaded_model.predict(input_data)[0]
+    with open("posture_model.pkl", "rb") as f:
+        loaded_model = load(f)
+        res = loaded_model.predict(input_data)[0]
 
-        return {"prediction": res}
-    except:
-        raise HTTPException(status_code=400, detail="Invalid JSON data")
+    return {"prediction": res}
+
 
 
 
